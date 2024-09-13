ddbdfec1561607c8ec67d8a19b64a6ca946e601c
ANUSHREE <anushreesrinivas18@gmail.com>
2024-09-13 10:15:58 +0530

    sem5_mod1

---
+++ javascript/function.js
@@ -0,0 +1,21 @@
+function oldversion()
+{
+    console.log("Hi I am old function")
+}
+oldversion()
+// new version function
+newversion = () =>{
+    console.log("Hi I am new function")
+}
+newversion()
+// old function with return
+function oldversion1(no1)
+{
+    return no1+10;
+}
+console.log(oldversion2(30))
+nweversion2 = (no1,no2)=>
+{
+    return no1+no2;
+}
+newversion(10,20)
